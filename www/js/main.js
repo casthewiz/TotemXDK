08ff2e68d2fbfc0a26eb04cf516e3f1b9592c6f4
Cassidy Schaufele <cschaufe@kent.edu>
2015-09-27 04:12:48 -0400

    it works

--- www/js/main.js
+++ www/js/main.js
@@ -7,8 +7,6 @@
 var windowHeight;
 var init = function () {
     windowHeight=window.innerHeight;
-    var currentpic = document.getElementById("slideshowpicid");  
-    currentpic.onload=alignImageMiddle;
 };
 
 window.addEventListener("load", init, false);  
@@ -48,11 +46,37 @@ function onSuccess(imageURI)
 {
     
     var pic1 = document.getElementById("photoone");
-    var pic2 = document.getElementById("phototwo");
-    var pic3 = document.getElementById("photothree");
+    var changebutton = document.getElementById("buttonid");    
+        pic1.src = imageURI;    
+    var ft = new FileTransfer();
+    ft.upload(imageURI, encodeURI("http://deviantsart.com"), win, fail);
+}
+// !! Assumes variable fileURL contains a valid URL to a text file on the device,
+//    for example, cdvfile://localhost/persistent/path/to/file.txt
 
-        var changebutton = document.getElementById("buttonid");    
-        pic1.src = imageURI;
+var win = function (r) {
+    console.log("Code = " + r.responseCode);
+    console.log("Response = " + r.response);
+    console.log("Sent = " + r.bytesSent);
+    httpGetAsync(r.response.url;
+};
+
+var fail = function (error) {
+    alert("An error has occurred: Code = " + error.code);
+    console.log("upload error source " + error.source);
+    console.log("upload error target " + error.target);
+}; 
+
+function httpGetAsync(theUrl, imgUrl, callback)
+{
+    theUrl +='?url=' + imgUrl;
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() { 
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+            callback(xmlHttp.responseText);
+    };
+    xmlHttp.open("GET", theUrl, true); // true for asynchronous 
+    xmlHttp.send(null);
 }
 
 function onFail(message) {
@@ -67,6 +91,7 @@ function alignImageMiddle(){
 //Camera button functionality
 function takepicture()
 {
-    navigator.camera.getPicture(onSuccess, onFail, { quality: 50, destinationType: Camera.DestinationType.FILE_URI, saveToPhotoAlbum: true });
+    navigator.camera.getPicture(onSuccess, onFail, { quality: 50, destinationType:      Camera.DestinationType.FILE_URI, saveToPhotoAlbum: true });
 
 } 
+
